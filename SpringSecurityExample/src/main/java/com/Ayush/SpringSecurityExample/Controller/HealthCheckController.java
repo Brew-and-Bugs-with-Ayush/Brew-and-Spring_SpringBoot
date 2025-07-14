52dbf68a86f4b3820a2f3c391708768c91a2dd9e
Ayush Gupta <121883530+CodeXayush79@users.noreply.github.com>
2025-07-15 00:28:15 +0530

    add SpringSecurityExample files

---
+++ SpringSecurityExample/src/main/java/com/Ayush/SpringSecurityExample/Controller/HealthCheckController.java
@@ -0,0 +1,14 @@
+package com.Ayush.SpringSecurityExample.Controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("")
+    public String message(HttpServletRequest request){
+        return "ok" + request.getSession().getId();
+    }
+}
