2817a04322919814e2cf516211dbe98618f0b628
Ayush Gupta <121883530+CodeXayush79@users.noreply.github.com>
2025-07-20 20:32:56 +0530

    add the use of SonarCube and SonarLint

--- RestApi_withDatabase/src/main/java/com/ayush/restapi_withdatabase/Entity/User.java
+++ RestApi_withDatabase/src/main/java/com/ayush/restapi_withdatabase/Entity/User.java
@@ -11,7 +11,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.ArrayList;
 import java.util.List;
-//import java.util.Date;
+
 
 @Data
 @Document(collection = "users")
